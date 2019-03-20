8006c887cf5ff4d4da88baf03d8357c3da821f9e
Kai R <kairichardson2000@gmail.com>
2019-03-20 13:02:53 -0700

    final 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,21 @@ var heroHP = 0;
 var oppHP = 0;
 var heroAtt = 0;
 var oppAtt = 0;
+var oppNum = 3;
 
 //deffender name
 var oppName = "";
 
 // Click events
 var idClicked = "";
-var firstClick = false;
-var secondClick = false;
+var playerEmpt = true;
+var oppEmpt = true;
+var gameEnd = false;
 
 // When the game starts, the player will choose a character by clicking on the fighter's picture. 
 $(".char").click(function (e) {
     idClicked = e.target.id;
-    if (!firstClick) {
+    if (playerEmpt) {
 
         if (idClicked === "obi") {
             $("#obiSpan").appendTo($("#second"));
@@ -32,34 +34,34 @@ $(".char").click(function (e) {
         if (idClicked === "maul") {
             $("#maulSpan").appendTo($("#second"));
         }
-        firstClick = true;
-        $("#first>.char").css({"border": "2px solid black", "background-color": "rgb(170, 36, 36)"});
+        playerEmpt = false;
+        $("#first>.char").css({ "border": "2px solid black", "background-color": "rgb(170, 36, 36)" });
         $("#instruct").text("Choose your Opponent");
         heroHP = parseInt($("#second>.char>.charHP").text());
         heroAtt = parseInt($("#second>span").attr("attp"));
         // console.log(heroHP);
         // console.log(heroAtt);
-        
-        
-    } else if (!secondClick) {
-        
+
+
+    } else if (oppEmpt) {
+
         if (idClicked === "obi") {
             $("#obiSpan").appendTo($("#third"));
         }
-        
+
         if (idClicked === "luke") {
             $("#lukeSpan").appendTo($("#third"));
         }
-        
+
         if (idClicked === "vader") {
             $("#vaderSpan").appendTo($("#third"));
         }
-        
+
         if (idClicked === "maul") {
             $("#maulSpan").appendTo($("#third"));
         }
-        secondClick = true;
-        $("#third>.char").css({"border": "2px solid green", "background-color": "black", "color": "rgb(173, 173, 173)"});
+        oppEmpt = false;
+        $("#third>.char").css({ "border": "2px solid green", "background-color": "black", "color": "rgb(173, 173, 173)" });
         $("#instruct").text("Get Ready to Fight!");
         oppHP = parseInt($("#third>.char>.charHP").text());
         oppAtt = parseInt($("#third>span").attr("cap"));
@@ -71,38 +73,65 @@ $(".char").click(function (e) {
 
 
 // The player will now be able to click the `attack` button.
-$("#fightBtn").click(function () {
-    $("#instruct").text("Ouch!");
-    // Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
-    // The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
-    
-    $("#attack").html("<h3>You attacked " + oppName + " for " + heroAtt + " damage!</h3>");
-    $("#defend").html("<h3>" + oppName + " attacked you back for " + oppAtt + " damage!</h3>");
-    
-    heroHP = heroHP - oppAtt;
-    oppHP = oppHP - heroAtt;
+$("#fight_btn").click(function () {
 
-    $("#second .charHP").text(heroHP);
-    $("#third .charHP").text(oppHP);
-    
-    heroAtt = heroAtt + parseInt($("#second>span").attr("attp"));
-    // console.log(heroHP);
-    // console.log(oppHP);
-    // console.log(heroAtt);
-    if (oppHP <= 0) {
-        $("#instruct").text("Nice job! Here come another one!");
-        $("#third>.char").remove();
-        $("#first>.char:first").appendTo($("#third"));
-        oppHP = parseInt($("#third>.char>.charHP").text());
-        oppAtt = parseInt($("#third>span").attr("cap"));
-        oppName = $("#third>.char>.name").text();
-    } else if (heroHP <= 0){
-        $("#instruct").text("Oh no! you lost!");
-        
+    if (!playerEmpt && !oppEmpt && !gameEnd) {
+        $("#instruct").text("Ouch!");
+        // Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
+        // The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
+
+        $("#attack").html("<h3>You attacked " + oppName + " for " + heroAtt + " damage!</h3>");
+        $("#defend").html("<h3>" + oppName + " attacked you back for " + oppAtt + " damage!</h3>");
+
+        heroHP = heroHP - oppAtt;
+        oppHP = oppHP - heroAtt;
+
+        $("#second .charHP").text(heroHP);
+        $("#third .charHP").text(oppHP);
+
+        heroAtt = heroAtt + parseInt($("#second>span").attr("attp"));
+        // console.log(heroHP);
+        // console.log(oppHP);
+        // console.log(heroAtt);
+        if (oppHP <= 0) {
+            $("#instruct").text("Nice job! Pick another opponant!");
+            $("#third>.char").remove();
+            oppEmpt = true;
+
+            oppHP = parseInt($("#third>.char>.charHP").text());
+            oppAtt = parseInt($("#third>span").attr("cap"));
+            oppName = $("#third>.char>.name").text();
+            oppNum--;
+
+            if (oppNum === 0) {
+                $("#instruct").text("You won! Great job!");
+                gameEnd = true;
+                $("#button_contain").append('<button type="button" class="btn btn-success" id="new_game_btn">New Game</button>');
+            }
+
+        } else if (heroHP <= 0) {
+            $("#instruct").text("Oh no! you lost!");
+            gameEnd = true;
+            $("#button_contain").append('<button type="button" class="btn btn-success" id="new_game_btn">New Game</button>');
+        }
     }
 
-
 });
+// restart button
+$("#new_game_btn").click(function (e) {
+    heroHP = 0;
+    oppHP = 0;
+    heroAtt = 0;
+    oppAtt = 0;
+    oppNum = 3;
+    playerEmpt = true;
+    oppEmpt = true;
+    gameEnd = false;
+
+    if (condition) {
+    }
+});
+
 // 3. The player will keep hitting the attack button in an effort to defeat their opponent.
 
 //    * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
